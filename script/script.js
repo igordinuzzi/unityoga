5937f7e0e23cdc64150cfca4592f4dae460c9bd8
Igor Dinuzzi <75853437+igordinuzzi@users.noreply.github.com>
2023-10-05 15:49:40 +0200

    feat: Added JavaScript for Accordion Function on Methodology page
    In this commit, I've uploaded JavaScript code to implement an accordion function on the Unity Yoga Collective webpage. This functionality will allow users to expand and collapse sections of content, providing a more organized and user-friendly experience when browsing through information about our yoga classes, schedules, or other relevant details.
    
    The accordion function enhances the interactivity and usability of our website, making it easier for visitors to find and access the information they need.
    
    #unityyogacollective #webdevelopment #javascript #accordion

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".accordion-button");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const content = this.nextElementSibling;
+
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
